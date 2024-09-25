cb7eb94f24e1c41bbd0d7cec49766c773c728d5c
sbottingota <saku.bottingota@gmail.com>
2024-09-25 17:40:31 +0100

    Fixed broken package and import statements

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/messages/TankCommandMessage.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/messages/TankCommandMessage.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.messages;
+package org.firstinspires.ftc.teamcode.util.roadrunner.messages;
 
 public final class TankCommandMessage {
     public long timestamp;
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/tuning/ManualFeedbackTuner.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/roadrunner/tuning/ManualFeedbackTuner.java
@@ -1,13 +1,13 @@
-package org.firstinspires.ftc.teamcode.tuning;
+package org.firstinspires.ftc.teamcode.util.roadrunner.tuning;
 
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.ftc.Actions;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
-import org.firstinspires.ftc.teamcode.MecanumDrive;
-import org.firstinspires.ftc.teamcode.TankDrive;
-import org.firstinspires.ftc.teamcode.ThreeDeadWheelLocalizer;
-import org.firstinspires.ftc.teamcode.TwoDeadWheelLocalizer;
+import org.firstinspires.ftc.teamcode.util.roadrunner.MecanumDrive;
+import org.firstinspires.ftc.teamcode.util.roadrunner.TankDrive;
+import org.firstinspires.ftc.teamcode.util.roadrunner.ThreeDeadWheelLocalizer;
+import org.firstinspires.ftc.teamcode.util.roadrunner.TwoDeadWheelLocalizer;
 
 public final class ManualFeedbackTuner extends LinearOpMode {
     public static double DISTANCE = 64;
