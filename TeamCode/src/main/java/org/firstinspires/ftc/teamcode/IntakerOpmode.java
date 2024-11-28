3bc87e6a07f167363c5e20304e29392ed64f6bd1
rMukha <rauf-m@outlook.com>
2024-11-28 11:02:53 +0000

    IntakerOpmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IntakerOpmode.java
@@ -0,0 +1,45 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
+import com.arcrobotics.ftclib.hardware.motors.Motor;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.util.inputs.PSButtons;
+
+
+@TeleOp
+
+public class IntakerOpmode extends OpMode {
+    private Motor turnMotor;
+    private GamepadEx gamePad;
+
+    @Override
+    public void init() {
+        turnMotor = new Motor(hardwareMap, "turn_motor");
+        gamePad = new GamepadEx(gamepad2);
+
+    }
+
+    @Override
+    public void loop() {
+        turnMotor.stopMotor();
+
+        gamePad.readButtons();
+        if (gamePad.wasJustPressed(PSButtons.CROSS)) {
+            turnMotor.set(1);
+        }
+        if (gamePad.wasJustPressed(PSButtons.CIRCLE)) {
+            turnMotor.set(-1);
+
+
+        }
+
+        if (gamePad.wasJustReleased(PSButtons.SQUARE)) {
+            turnMotor.set(0);
+        }
+
+
+    }
+
+}
