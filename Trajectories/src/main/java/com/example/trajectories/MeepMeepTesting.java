2c0f9908feec8742a16952d6e6265012b654d1f3
Rowan <14rmackay@brightoncollege.net>
2025-01-25 13:52:33 +0000

    Update MeepMeepTesting.java

--- Trajectories/src/main/java/com/example/trajectories/MeepMeepTesting.java
+++ Trajectories/src/main/java/com/example/trajectories/MeepMeepTesting.java
@@ -1,6 +1,7 @@
 package com.example.trajectories;
 
 import com.acmerobotics.roadrunner.Pose2d;
+import com.acmerobotics.roadrunner.Vector2d;
 import com.noahbres.meepmeep.MeepMeep;
 import com.noahbres.meepmeep.roadrunner.DefaultBotBuilder;
 import com.noahbres.meepmeep.roadrunner.entity.RoadRunnerBotEntity;
@@ -11,26 +12,51 @@ public class MeepMeepTesting {
 
         MeepMeep meepMeep = new MeepMeep(800);
 
-        RoadRunnerBotEntity myBot = new DefaultBotBuilder(meepMeep)
+
+        RoadRunnerBotEntity myBot1 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 1)
+                .build();
+        RoadRunnerBotEntity myBot2 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+        RoadRunnerBotEntity myBot3 = new DefaultBotBuilder(meepMeep)
+                // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
+                .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
+                .build();
+        RoadRunnerBotEntity myBot4 = new DefaultBotBuilder(meepMeep)
                 // Set bot constraints: maxVel, maxAccel, maxAngVel, maxAngAccel, track width
                 .setConstraints(60, 60, Math.toRadians(180), Math.toRadians(180), 15)
                 .build();
 
-        myBot.runAction(myBot.getDrive().actionBuilder(new Pose2d(0, 0, 0))
-                .lineToX(30)
-                .turn(Math.toRadians(90))
-                .lineToY(30)
-                .turn(Math.toRadians(90))
-                .lineToX(0)
-                .turn(Math.toRadians(90))
-                .lineToY(0)
-                .turn(Math.toRadians(90))
+        myBot1.runAction(myBot1.getDrive().actionBuilder(new Pose2d(0, 25, 0))
+                .splineTo(new Vector2d(56, 56), 0)
+                        .splineToSplineHeading(new Pose2d(0, 25, 0),0)
+                        .build());
+        myBot2.runAction(myBot2.getDrive().actionBuilder(new Pose2d(0, 25, 0))
+                .splineTo(new Vector2d(-56, 56), 0)
+                .splineToSplineHeading(new Pose2d(0, 25, 0),0)
+                .build());
+        myBot3.runAction(myBot3.getDrive().actionBuilder(new Pose2d(0, -25, 0))
+                .splineTo(new Vector2d(-56, -56), 0)
+                .splineToSplineHeading(new Pose2d(0, -25, 0),0)
+                .build());
+        myBot4.runAction(myBot4.getDrive().actionBuilder(new Pose2d(0, -25, 0))
+                .splineTo(new Vector2d(56, -56), 0)
+                .splineToSplineHeading(new Pose2d(0, -25, 0),0)
                 .build());
 
-        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_LIGHT)
+
+
+
+        meepMeep.setBackground(MeepMeep.Background.FIELD_INTO_THE_DEEP_JUICE_DARK)
                 .setDarkMode(true)
                 .setBackgroundAlpha(0.95f)
-                .addEntity(myBot)
+                .addEntity(myBot1)
+                .addEntity(myBot2)
+                .addEntity(myBot3)
+                .addEntity(myBot4)
                 .start();
     }
 }
