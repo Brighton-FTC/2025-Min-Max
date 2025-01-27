ff42f4acb4e434126aad41e48af7205c1b98d378
Rowan <14rmackay@brightoncollege.net>
2025-01-27 16:56:37 +0000

    Update TrajectoriesTeleop.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrajectoriesTeleop.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrajectoriesTeleop.java
@@ -4,8 +4,10 @@ import com.acmerobotics.roadrunner.Action;
 import com.acmerobotics.roadrunner.Pose2d;
 import com.acmerobotics.roadrunner.Trajectory;
 import com.acmerobotics.roadrunner.ftc.Actions;
+import com.arcrobotics.ftclib.gamepad.GamepadEx;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 
+import org.firstinspires.ftc.teamcode.util.inputs.PSButtons;
 import org.firstinspires.ftc.teamcode.util.roadrunner.MecanumDrive;
 
 public class TrajectoriesTeleop extends LinearOpMode {
@@ -19,11 +21,23 @@ public class TrajectoriesTeleop extends LinearOpMode {
         Action basketToCenter = drive.actionBuilder(new Pose2d(56, 56, 0))
                 .splineToSplineHeading(new Pose2d(0, 25, 0), 0).build();
 
+        GamepadEx gamepad = new GamepadEx(gamepad1);
+
         waitForStart();
 
+        boolean isAtCenter = true;
+
         while(!isStopRequested()) {
-            Actions.runBlocking(centerToBasket);
-            Actions.runBlocking(basketToCenter);
+            gamepad.readButtons();
+            if (gamepad.wasJustPressed(PSButtons.CIRCLE) && isAtCenter) {
+                Actions.runBlocking(centerToBasket);
+                isAtCenter = !isAtCenter;
+            }
+            if(gamepad.wasJustPressed(PSButtons.CIRCLE) && isAtCenter == false){
+                Actions .runBlocking(basketToCenter);
+                isAtCenter = !isAtCenter;
+            }
+            sleep(20);
         }
     }
 }
