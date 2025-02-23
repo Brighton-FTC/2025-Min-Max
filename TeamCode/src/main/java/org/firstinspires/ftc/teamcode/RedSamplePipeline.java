bbde1ffff947f9e21eac774e64c66b50f7e51e0b
sbottingota <saku.bottingota@gmail.com>
2025-02-23 20:16:23 +0000

    Filled in approximate values for pipelines using EOCV-SIM

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedSamplePipeline.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/RedSamplePipeline.java
@@ -3,8 +3,8 @@ package org.firstinspires.ftc.teamcode;
 import org.opencv.core.Scalar;
 
 public class RedSamplePipeline extends SamplePipeline {
-    public static final Scalar LOWER = new Scalar(0, 0, 0);
-    public static final Scalar UPPER = new Scalar(255, 255, 255);
+    public static final Scalar LOWER = new Scalar(140, 100, 160);
+    public static final Scalar UPPER = new Scalar(210, 225, 240);
 
     public RedSamplePipeline() {
         super(LOWER, UPPER);
